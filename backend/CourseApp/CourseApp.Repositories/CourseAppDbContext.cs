ae2a5036f6b0d9b10897310389f8a4016fb7a0cc
Beyza Cebeci <bcebecii@outlook.com>
2024-12-28 17:15:34 +0300

    Layers were created, and DbContext was added.

---
+++ backend/CourseApp/CourseApp.Repositories/CourseAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CourseApp.Repositories.Courses;
+using CourseApp.Repositories.Orders;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+
+namespace CourseApp.Repositories;
+
+public class CourseAppDbContext(DbContextOptions<CourseAppDbContext> options) : DbContext(options)
+{
+    DbSet<Course> Courses { get; set; } = default!;
+    DbSet<Order> Orders { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
---
+++ backend/CourseApp/CourseApp.Repositories/Courses/CourseConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace CourseApp.Repositories.Courses;
+
+public class CourseConfiguration : IEntityTypeConfiguration<Course>
+{
+    public void Configure(EntityTypeBuilder<Course> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.Description).IsRequired().HasMaxLength(200);
+        builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(16,2)");
+
+    }
+}
+
